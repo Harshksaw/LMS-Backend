378b1f88abee9e044bbefc61b865b88b113042b4
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-11-17 19:23:58 +0530

    Course added

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -28,67 +28,131 @@ export const getAllCourses = asyncHandler(async (_req, res, next) => {
  * @ROUTE @POST {{URL}}/api/v1/courses
  * @ACCESS Private (admin only)
  */
-export const createCourse = asyncHandler(async (req, res, next) => {
+// export const createCourse = asyncHandler(async (req, res, next) => {
+//   const { title, description, category, createdBy } = req.body;
+
+//   if (!title || !description || !category || !createdBy) {
+//     return next(new AppError('All fields are required', 400));
+//   }
+
+
+//   const course = await Course.create({
+//     title,
+//     description,
+//     category,
+//     createdBy,
+//     thumbnail: {
+//       public_id: 'Dummy',
+//       secure_url: 'Dummy',
+//     },
+    
+//   });
+
+
+//   if (!course) {
+//     return next(
+//       new AppError('Course could not be created, please try again', 400)
+//     );
+//   }
+
+//   // Run only if user sends a file
+//   if (req.file) {
+//     try {
+//       const result = await cloudinary.v2.uploader.upload(req.file.path, {
+//         folder: 'lms', // Save files in a folder named lms
+//       });
+
+//       // If success
+//       if (result) {
+//         // Set the public_id and secure_url in array
+//         course.thumbnail.public_id = result.public_id;
+//         course.thumbnail.secure_url = result.secure_url;
+//       }
+
+//       // After successful upload remove the file from local storage
+//       fs.rm(`uploads/${req.file.filename}`);
+//     } catch (error) {
+//       // Empty the uploads directory without deleting the uploads directory
+//       for (const file of await fs.readdir('uploads/')) {
+//         await fs.unlink(path.join('uploads/', file));
+//       }
+
+//       // Send the error message
+//       return next(
+//         new AppError(
+//           JSON.stringify(error) || 'File not uploaded, please try again',
+//           400
+//         )
+//       );
+//     }
+//   }
+
+//   // Save the changes
+//   await course.save();
+
+//   res.status(201).json({
+//     success: true,
+//     message: 'Course created successfully',
+//     course,
+//   });
+// });
+
+export const createCourse = async(req, res, next) => {
+
   const { title, description, category, createdBy } = req.body;
 
   if (!title || !description || !category || !createdBy) {
-    return next(new AppError('All fields are required', 400));
-  }
-
-  const course = await Course.create({
-    title,
-    description,
-    category,
-    createdBy,
-  });
-
-  if (!course) {
-    return next(
-      new AppError('Course could not be created, please try again', 400)
-    );
-  }
-
-  // Run only if user sends a file
-  if (req.file) {
-    try {
-      const result = await cloudinary.v2.uploader.upload(req.file.path, {
-        folder: 'lms', // Save files in a folder named lms
-      });
-
-      // If success
-      if (result) {
-        // Set the public_id and secure_url in array
-        course.thumbnail.public_id = result.public_id;
-        course.thumbnail.secure_url = result.secure_url;
-      }
-
-      // After successful upload remove the file from local storage
-      fs.rm(`uploads/${req.file.filename}`);
-    } catch (error) {
-      // Empty the uploads directory without deleting the uploads directory
-      for (const file of await fs.readdir('uploads/')) {
-        await fs.unlink(path.join('uploads/', file));
-      }
-
-      // Send the error message
       return next(
-        new AppError(
-          JSON.stringify(error) || 'File not uploaded, please try again',
-          400
-        )
-      );
-    }
+          new AppError('ALl fields are required->', 400)
+      )
+  }
+  const course = await Course.create({
+      title,
+      description,
+      category,
+      createdBy,
+      thumbnail: {
+          public_id: 'Dummy',
+          secure_url: 'Dummy',
+      },
+  })
+  if (!course) {
+      return next(
+          new AppError('Course could not be created please try again')
+      )
+  }
+  try {
+
+
+      if (req.file) {
+          const result = await cloudinary.v2.uploader.upload(req.file.path,
+              {
+                  folder: 'Lms'
+              })
+          if (result) {
+              course.thumbnail.public_id = result.public_id;
+              course.thumbnail.secure_url = result.secure_url;
+
+          }
+          //delete file from local machine
+          fs.rm(`uploads/${req.file.filename}`)
+
+
+      }
+      await course.save()
+
+      res.status(200).json({
+          success: true,
+          message: 'Course created successfully ',
+          course,
+      });
+  } catch (e) {
+      return next(
+          new AppError(e.message, 'Course could not created please try again later', 500)
+      )
   }
 
-  // Save the changes
-  await course.save();
-
-  res.status(201).json({
-    success: true,
-    message: 'Course created successfully',
-    course,
-  });
-});
+}
 
 /**
  * @GET_LECTURES_BY_COURSE_ID
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
 import {
     createCourse,
-
     getAllCourses,
-
-
     addLectureToCourseById,
     getLecturesByCourseId,
     deleteCourseById,
-
 } from "../controllers/course.controller.js";
 import {
     authorizeRoles,
@@ -17,29 +13,32 @@ import {
 } from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
+
 const router = new Router();
 
 router
     .route("/")
-
     .get(getAllCourses)
-    .post(isLoggedIn, 
-        authorizeRoles('ADMIN'),
+    .post(
+        isLoggedIn,
+        authorizeRoles("ADMIN"),
         upload.single("thumbnail"),
-        createCourse)
-    // .delete(isLoggedIn, authorizeRoles('ADMIN'), rem)
+        createCourse
+
+    );
+
+// .delete(isLoggedIn, authorizeRoles('ADMIN'), rem)
 
 router
     .route("/:id")
     .get(isLoggedIn, authorizeSubscribers, getLecturesByCourseId)
     .post(
         isLoggedIn,
-        authorizeRoles('ADMIN'),
-        upload.single('lecture'),
+        authorizeRoles("ADMIN"),
+        upload.single("lecture"),
         addLectureToCourseById
-      )
-    .delete(isLoggedIn, authorizeRoles('ADMIN'), deleteCourseById)
-    // .put(isLoggedIn, authorizeRoles('ADMIN'), updateCourseById);
-
+    )
+    .delete(isLoggedIn, authorizeRoles("ADMIN"), deleteCourseById);
+// .put(isLoggedIn, authorizeRoles('ADMIN'), updateCourseById);
 
 export default router;
